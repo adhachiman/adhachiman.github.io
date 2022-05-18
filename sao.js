8c27020339485f1778d3e7047547c37b09d03394
adhachiman <adhamzero7@gmail.com>
2022-05-19 01:17:17 +0800

    add new file

---
+++ sao.js
@@ -0,0 +1,165 @@
+function _e(id) {
+return document.getElementById(id);
+}
+
+function displayA(number){
+    if(number == 1)
+    {
+        document.getElementById("container").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container").innerHTML = "★★★★★";
+    }
+
+}
+
+function displayB(number){
+    if(number == 1)
+    {
+        document.getElementById("container1").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container1").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container1").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container1").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container1").innerHTML = "★★★★★";
+    }
+
+}
+
+function displayC(number){
+    if(number == 1)
+    {
+        document.getElementById("container2").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container2").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container2").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container2").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container2").innerHTML = "★★★★★";
+    }
+
+}
+
+function displayD(number){
+    if(number == 1)
+    {
+        document.getElementById("container3").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container3").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container3").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container3").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container3").innerHTML = "★★★★★";
+    }
+
+}
+
+function displayE(number){
+    if(number == 1)
+    {
+        document.getElementById("container4").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container4").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container4").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container4").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container4").innerHTML = "★★★★★";
+    }
+
+}
+
+function displayF(number){
+    if(number == 1)
+    {
+        document.getElementById("container5").innerHTML = "★";
+        
+    }
+   
+   else if(number == 2)
+    {
+        document.getElementById("container5").innerHTML = "★★";  
+        
+    }
+    else if(number == 3)
+    {
+        document.getElementById("container5").innerHTML = "★★★";  
+    }
+    else if(number == 4)
+    {
+        document.getElementById("container5").innerHTML = "★★★★";
+    }
+    else if(number == 5)
+    {
+        document.getElementById("container5").innerHTML = "★★★★★";
+    }
+
+}
